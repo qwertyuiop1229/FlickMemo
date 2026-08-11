e4b57bb45940fd294fe341d495f83c8ea3574db6
qwe <hyuuga1229@gmail.com>
2026-08-11 22:05:56 +0900

    auto deploy: update code and version

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ import 'prismjs/components/prism-json';
 import { FileTransferManager } from './fileTransfer.js';
 
 // ★ アプリ内に直接埋め込まれたバージョン定数（bump.jsでデプロイ時に自動書き換え）
-const APP_VERSION = "1.3.51";
+const APP_VERSION = "1.3.52";
 
 // ⚠️ ご自身のキーを入れてください
 const firebaseConfig = {
